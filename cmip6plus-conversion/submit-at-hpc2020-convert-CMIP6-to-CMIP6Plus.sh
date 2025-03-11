dc25c0bc83bee69f7493f8f02740d60484d80d05
treerink <thomas.reerink.knmi@gmail.com>
2025-03-12 00:13:11 +0100

    Enhance flexibility in the submit script calling with the convert-cmip6-to-cmip6plus.sh options.

--- cmip6plus-conversion/submit-at-hpc2020-convert-CMIP6-to-CMIP6Plus.sh
+++ cmip6plus-conversion/submit-at-hpc2020-convert-CMIP6-to-CMIP6Plus.sh
@@ -18,29 +18,31 @@
 # CMIP6DIR        is the directory with the cmorised data, for instance a test case: cmorMDfixer-test-data/test-set-01/CMIP6/
 # METADATAFILE    is the name with the meta data correction, for instance: 
 
- if [ "$#" -eq 1 ]; then
+ if [ ! "$#" = 0 ]; then
 
-   CMIP6DIR=$1
+   argument_list=$*
+   CMIP6DIR="${@: -1}"
 
-   if [ ! -d ${CMIP6DIR} ]; then echo "Error: Directory: ${CMIP6DIR}, does not exist. Abort $0" >&2; exit 1; fi
+   if [ ! -d ${CMIP6DIR} ]; then echo -e "\e[1;31m Error:\e[0m"" Directory: ${CMIP6DIR}, does not exist. Abort $0" >&2; exit 1; fi
 
-   if [   -z ${CMIP6DIR} ]; then echo "Error: Empty directory, no cmorised data in: ${CMIP6DIR}. Abort $0" >&2; exit 1; fi
+   if [   -z ${CMIP6DIR} ]; then echo -e "\e[1;31m Error:\e[0m"" Empty directory, no cmorised data in: ${CMIP6DIR}. Abort $0" >&2; exit 1; fi
 
    source ${PERM}/mamba/etc/profile.d/conda.sh
    conda activate cmorMDfixer
 
    export HDF5_USE_FILE_LOCKING=FALSE
 
-   ./convert-cmip6-to-cmip6plus.sh $CMIP6DIR
+   ./convert-cmip6-to-cmip6plus.sh ${argument_list}
 
  else
   echo
   echo " Illegal number of arguments: this script itself requires one argument: the path of the CMIP6 cmorised data, e.g.:"
-  echo "  sbatch --qos=nf --cpus-per-task=2   --account=nlchekli $0 cmorMDfixer-test-data/test-set-01/CMIP6/"
-  echo "  sbatch --qos=nf --cpus-per-task=50  --account=nlchekli $0 /scratch/nktr/cmorised-results/test-all-trunk/t001/v046/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-ESM-1/esm-hist/"
-  echo "  sbatch --qos=np --cpus-per-task=128 --account=nlchekli $0 /scratch/nktr/cmorised-results/test-all-trunk/t001/v003/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3/piControl/"
+  echo "  sbatch --qos=nf --cpus-per-task=2   --account=nlchekli $0 -o -s EC-Earth3-ESM-1 ../cmorMDfixer-test-data/test-set-01/CMIP6/"
+  echo "  sbatch --qos=nf --cpus-per-task=2   --account=nlchekli $0 -o                    ../cmorMDfixer-test-data/test-set-02/CMIP6/"
+  echo "  sbatch --qos=nf --cpus-per-task=16  --account=nlchekli $0 -o                    -p /scratch/nktr/cmorised-results/converted-to-cmip6plus/ /scratch/nktr/cmorised-results/test-all-trunk/t001/v046/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3-ESM-1/esm-hist/"
+  echo "  sbatch --qos=nf --cpus-per-task=16  --account=nlchekli $0 -o -s EC-Earth3-ESM-1 -p /scratch/nktr/cmorised-results/converted-to-cmip6plus/ /scratch/nktr/cmorised-results/test-all-trunk/t001/v003/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3/piControl/"
+  echo "  sbatch --qos=np --cpus-per-task=128 --account=nlchekli $0 -o -s EC-Earth3-ESM-1 -p /scratch/nktr/cmorised-results/converted-to-cmip6plus/ /scratch/nktr/cmorised-results/test-all-trunk/t001/v003/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3/piControl/"
   echo
-  echo " Available accounts for ${USER} on hpc2020:"
-  account -l $USER
+  echo " Available accounts for ${USER} on hpc2020: ${account_info}"
   echo
  fi
